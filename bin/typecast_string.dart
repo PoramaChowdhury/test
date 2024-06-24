771c953b850878b28847ea4dbb0efe7a208eb881
pcpri <cse_182210012101023@lus.ac.bd>
2024-06-24 21:28:30 +0600

    typecast,String Interpolation

---
+++ bin/typecast_string.dart
@@ -0,0 +1,39 @@
+//type_casting
+
+/*
+void main(){
+  double value = 40.05;
+  int a = value.toInt();
+  String x = a.toString();
+  print(a);
+  value = a.toDouble();
+  print(x.runtimeType);
+  print(value);
+  x = value.toString();
+  print(x);
+
+
+  String numValue = '34';
+  int id = int.parse(numValue);
+  print(id.runtimeType);
+
+  String gpa = '3.40';
+  double cg = double.parse(gpa);
+  print(cg);
+
+}*/
+
+//String Interpolation
+/*
+void main(){
+  int age = 30;
+  String b = 'ASS';
+  String c = 'kmmmm';
+  String s = b+c;
+  String msg = 'My age is $age $s';
+  print(msg);
+
+  String result = 'Addition ${12+45+67}';
+  print(result);
+
+}*/
