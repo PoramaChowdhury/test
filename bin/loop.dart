ecd4024b031adcbd5f4198346af42f483de0af82
pcpri <cse_182210012101023@lus.ac.bd>
2024-06-24 21:58:36 +0600

    for,for in,forEach

--- bin/loop.dart
+++ bin/loop.dart
@@ -1,6 +1,8 @@
 //for,while,do-while loop
-/*void main(){
-  //for
+
+/*
+void main(){
+  //for loop
  for(int i=0; i<=10;i++){
    if(i==5){
       continue;
@@ -29,65 +31,32 @@ void greeting(){
   print('do u wanna tear or coffee');
   print('good bye');
 }*/
+
 //LIST AND MAP loop
 
-void main(){
-  List<String> Students = [
-    'Afrin',
-    'pc',
-    'er',
-    'err'
-  ];
-  for(int i = 0; i<Students.length;i++){
+void main() {
+  List<String> Students = ['Afrin', 'pc', 'er', 'err'];
+  for (int i = 0; i < Students.length; i++) {
     print('Students $i: ${Students[i]}');
   }
   //for in
-  /*for(String Student in Students) {
+  for (String Student in Students) {
     print('Student name : $Students');
-  }*/
+  }
   //forEach
-  Students.forEach((stu){
+  Students.forEach((stu) {
     print(stu);
   });
 
-  Map<String, Map<String,String>> friends = {
-    'iram' : {
-      'address': 'dhaka',
-       'age': '30'},
-    'subin' : {
-      'address': 'Syl',
-      'age': '38' }
-    };
-  for(String key in friends.keys){
-    print('My friend name is $key. address: ${friends[key]!['address']} Age: ${friends[key]!['age']}');
+  Map<String, Map<String, String>> friends = {
+    'iram': {'address': 'dhaka', 'age': '30'},
+    'subin': {'address': 'Syl', 'age': '38'}
+  };
+  for (String key in friends.keys) {
+    print(
+        'My friend name is $key. address: ${friends[key]!['address']},Age: ${friends[key]!['age']}');
   }
-  for(Map<String, String> details in friends.values){
+  for (Map<String, String> details in friends.values) {
     print(details);
   }
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
